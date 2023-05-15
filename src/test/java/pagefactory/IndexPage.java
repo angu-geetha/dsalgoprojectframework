dc94fae6400b31560068c69fcc9f8976351db54a
geetha <angu.geetha@gmail.com>
2023-05-15 00:02:43 -0400

    commit on 15th may with config reader

--- src/test/java/pagefactory/IndexPage.java
+++ src/test/java/pagefactory/IndexPage.java
@@ -7,10 +7,12 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import driverFactory.Driver_Factory;
+import utilities.Configreader;
+import utilities.LoggerLoad;
 
 public class IndexPage {
 	WebDriver chromedriver ;
-	final String homePageUrl = "https://dsportalapp.herokuapp.com/" ;
+	final String homePageUrl = Configreader.getProperty("url"); ;
 	
 	@FindBy(xpath = "//button[text()='Get Started']") WebElement GetStarted;
 	//By getStartedButton = By.xpath("//button[text()='Get Started']");
@@ -22,11 +24,16 @@ public class IndexPage {
 	}
 	
 	public void clickGetStarted() {
+		LoggerLoad.logInfo("Entering Method : IndexPage.clickGetStarted");
 		GetStarted.click();
+		LoggerLoad.logInfo("Exiting Method : IndexPage.clickGetStarted");
 	}
 	
 	public void openDsAlgoPage() {
+		LoggerLoad.logInfo("Entering Method : IndexPage.openDsAlgoPage");
+		LoggerLoad.logDebug("The Home page URL is "+ homePageUrl);
 		chromedriver.get(homePageUrl);
+		LoggerLoad.logInfo("Exiting Method : IndexPage.openDsAlgoPage");
 		
 	}
 
